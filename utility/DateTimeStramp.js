4e4702ebb7b72f2559306cfd2701335587f2c844
ukpono-j <ukponoakpan270@gmail.com>
2023-10-23 00:29:26 +0100

    just finished end point for transaction details and  cancelled transaction

--- utility/DateTimeStramp.js
+++ utility/DateTimeStramp.js
@@ -1,38 +1,5 @@
+import { formatDistanceToNow } from 'date-fns';
+
 export function formatCreatedAt(createdAt) {
-  const now = new Date();
-  const timestamp = new Date(createdAt);
-  const seconds = Math.floor((now - timestamp) / 1000);
-
-  if (seconds < 60) {
-    return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
-  }
-
-  const minutes = Math.floor(seconds / 60);
-  if (minutes < 60) {
-    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
-  }
-
-  const hours = Math.floor(minutes / 60);
-  if (hours < 24) {
-    return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
-  }
-
-  const days = Math.floor(hours / 24);
-  if (days < 7) {
-    return `${days} day${days !== 1 ? "s" : ""} ago`;
-  }
-
-  const weeks = Math.floor(days / 7);
-  if (weeks < 4.34812) {
-    // approximately the number of weeks in a month
-    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`;
-  }
-
-  const months = Math.floor(days / 30.4369); // approximately the number of days in a month
-  if (months < 12) {
-    return `${months} month${months !== 1 ? "s" : ""} ago`;
-  }
-
-  const years = Math.floor(months / 12);
-  return `${years} year${years !== 1 ? "s" : ""} ago`;
+  return formatDistanceToNow(new Date(createdAt), { addSuffix: true });
 }
