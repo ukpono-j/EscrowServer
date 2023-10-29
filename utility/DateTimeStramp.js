615b1a5548ebd23675b0168f4444a7807021f9d1
ukpono-j <ukponoakpan270@gmail.com>
2023-10-29 17:36:14 +0100

    made new endpoint and  changed some old ones

--- utility/DateTimeStramp.js
+++ utility/DateTimeStramp.js
@@ -1,5 +1,14 @@
 import { formatDistanceToNow } from 'date-fns';
 
+// export function formatCreatedAt(createdAt) {
+//   return formatDistanceToNow(new Date(createdAt), { addSuffix: true });
+// }
+
 export function formatCreatedAt(createdAt) {
-  return formatDistanceToNow(new Date(createdAt), { addSuffix: true });
+  try {
+    return formatDistanceToNow(new Date(createdAt), { addSuffix: true });
+  } catch (error) {
+    console.error("Error formatting createdAt:", error);
+    return "Invalid Date"; // or handle it in a way suitable for your application
+  }
 }
