6135a4c0b64f0a2cb6b2eeb6e480e3d6c748affd
ukpono-j <ukponoakpan270@gmail.com>
2023-10-31 15:12:27 +0100

    added 0.0.0.0 to the listen port

--- index.js
+++ index.js
@@ -544,6 +544,6 @@ const getParticipantsForTransaction = async (transactionId) => {
 // });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',  () => {
   console.log(`Server is running on port ${PORT}`);
 });
