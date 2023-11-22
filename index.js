765b1ed0ff1fe4ecc41719e07b00439695855f2d
ukpono-j <ukponoakpan270@gmail.com>
2023-11-22 15:08:33 +0100

    fixing the static file path

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 // app.use(express.static("uploads/images/"));
-app.use("/images", express.static("uploads/images"));
+// app.use("/images", express.static("uploads/images"));
+app.use("/images", express.static("./uploads/images"));
+
 
 const corsOptions = {
   origin: [
