ca704222c460f7bd24af7c13ad695950c53d018d
ukpono-j <ukponoakpan270@gmail.com>
2023-11-22 15:16:09 +0100

    fixing the static file path111

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 // app.use(express.static("uploads/images/"));
 // app.use("/images", express.static("uploads/images"));
-app.use("/images", express.static("./uploads/images"));
 
+app.use("/images", express.static("./uploads/images"));
 
 const corsOptions = {
   origin: [
