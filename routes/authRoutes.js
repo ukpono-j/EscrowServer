c88efd632828d372db9cb6a1115d4650f35ea904
ukpono-j <ukponoakpan270@gmail.com>
2025-08-29 17:49:24 +0100

    i had to undo the auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController');
 
 router.post('/login', authController.login);
 router.post('/register', authController.register);
-router.post('/verify-email', authController.verifyEmail);
-router.post('/resend-verification', authController.resendVerification);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 
